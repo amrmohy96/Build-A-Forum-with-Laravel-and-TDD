b0c0836f393de56e507fb09e2e7d883b975c5283
amrmohy96 <damrmohy@gmail.com>
2021-01-27 08:10:34 +0000

    add threads endpoint

--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ThreadTest extends TestCase
@@ -11,7 +9,7 @@ class ThreadTest extends TestCase
     /** @test */
     public function a_user_can_browse_threads()
     {
-        $response = $this->get('/');
+        $response = $this->get('/threads');
 
         $response->assertStatus(200);
     }
